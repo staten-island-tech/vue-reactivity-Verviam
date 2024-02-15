fc9e6e047981bf43300065bbcd703549b521704f
Verviam <gaoevan37@gmail.com>
2024-02-15 08:32:50 -0500

    asdf

--- vue-project/src/components/WebsitesCard.vue
+++ vue-project/src/components/WebsitesCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div><h1>{{ shoppincartarraynames }}</h1>
+  <div><h1>{{ website.name }}</h1>
   
   </div>
 </template>
--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -14,9 +14,14 @@
 import websites from '@/components/WebsitesCard.vue';
 const websites = [
   {
-    name: 
-    price:
-    img:
+    url: "OfZenAndComputing.com",
+    type:
+    monetization:
+    netProfit:
+    price: "$4,823 /mo",
+    description: "Passive & Growing 4yr old Tech & Gaming Blog with High DA & Extensive Opportunities for Growth, Generating $5,864/m in Net Profit w/ Diversified Income Streams",
+    img: "https://d2jafhvbn4akdi.cloudfront.net/webp_c6f5edd0-5809-43bd-9ad6-807ad5c2f4ef.png",
+    siteAge: 
   }
 ]
 
